e0d96912d39faa46a276bbfd857937e411e5079d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-12-27 11:16:14 +0700

    Fix style

--- 04_check_dimensions.cu
+++ 04_check_dimensions.cu
@@ -1,17 +1,18 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
-__global__ void check_index() {
+__global__ void check_index()
+{
     printf("threadIdx: (%d,%d,%d) blockIdx: (%d,%d,%d) blockDim: (%d,%d,%d) gridDim: (%d,%d,%d)\n",
             threadIdx.x, threadIdx.y, threadIdx.z, 
             blockIdx.x, blockIdx.y, blockIdx.z,
             blockDim.x, blockDim.y, blockDim.z,
             gridDim.x, gridDim.y, blockDim.z
     );
-    //printf("\n");
 }
 
-int main( int argc, char **argv) {
+int main( int argc, char **argv)
+{
 
     int Ndata = 10;
 
